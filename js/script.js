be60074da9754adfda98225601429a00e99c1d96
Nikita <nikitbutko8@gmail.com>
2022-10-19 22:36:33 +0200

    end clickHandler

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function titleClickHandler(event) {
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
   /* [DONE]remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -11,9 +12,9 @@ function titleClickHandler(event) {
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS]add class 'active' to the clicked link */
-  const clickedElement = this;
+  /* [DONE]add class 'active' to the clicked link */
   clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
   /* [DONE]remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
@@ -22,11 +23,17 @@ function titleClickHandler(event) {
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle', targetArticle);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
